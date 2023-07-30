27e80e8ce2be4f5ac1bb18274cb25c0114a5d560
Matte <mattias.johansson@funnel.io>
2023-07-30 16:56:27 +0200

    some refactoring

--- magnifying-glass/src/main.rs
+++ magnifying-glass/src/main.rs
@@ -1,11 +1,11 @@
-use image::GenericImageView;
+use image::{DynamicImage, GenericImageView};
 use pixel_canvas::{input::MouseState, Canvas, Color};
 
 fn main() {
     let data = include_bytes!("../assets/test.jpeg");
     let bg = image::load_from_memory(data).unwrap();
     let canvas = Canvas::new(500, 500)
-        .title("Tile")
+        .title("Magnifying Glass")
         .state(MouseState::new())
         .input(MouseState::handle_input);
     canvas.render(move |mouse, image| {
@@ -13,59 +13,48 @@ fn main() {
         let height = image.height();
         for (y, row) in image.chunks_mut(width).enumerate() {
             for (x, pixel) in row.iter_mut().enumerate() {
-                let x = x as i32;
-                let y = y as i32;
-                let dx = x - mouse.x;
-                let dy = y - mouse.y;
-                let a = x.abs_diff(mouse.x).pow(2);
-                let b = y.abs_diff(mouse.y).pow(2);
-                let distance = ((a + b) as f64).sqrt();
-                if distance > 75. {
-                    let sample_x = x * bg.width() as i32 / width as i32;
-                    let sample_y = y * bg.height() as i32 / height as i32;
-                    let rgba = bg.get_pixel(
-                        (sample_x as u32).clamp(0, bg.width() - 1),
-                        (sample_y as u32).clamp(0, bg.height() - 1),
-                    );
-                    *pixel = Color {
-                        r: rgba.0[0],
-                        g: rgba.0[1],
-                        b: rgba.0[2],
-                    };
+                let distance = distance(mouse, x, y);
+                let [x, y] = if distance > 75. {
+                    [x as i32, y as i32]
                 } else {
-                    let d_norm = distance / 75.0;
-                    let g = (-0.5 + d_norm).tan();
-                    let x = x as f64 + dx as f64 * g;
-                    let y = y as f64 + dy as f64 * g;
-                    let x = x.clamp(0., width as f64);
-                    let y = y.clamp(0., height as f64);
-                    let sample_x = x as i32 * bg.width() as i32 / width as i32;
-                    let sample_y = y as i32 * bg.height() as i32 / height as i32;
-                    let sample_x = sample_x.clamp(0, bg.width() as i32 - 1);
-                    let sample_y = sample_y.clamp(0, bg.height() as i32 - 1);
-                    let rgba = bg.get_pixel(sample_x as u32, sample_y as u32);
-                    *pixel = Color {
-                        r: rgba.0[0],
-                        g: rgba.0[1],
-                        b: rgba.0[2],
-                    };
-                }
+                    let (dx, dy) = project(
+                        x as f64 - mouse.x as f64,
+                        y as f64 - mouse.y as f64,
+                        distance,
+                    );
+                    [x as i32 + dx, y as i32 + dy]
+                };
+                *pixel = sample(&bg, width as u32, height as u32, x as u32, y as u32);
             }
         }
     });
 }
 
-#[test]
-fn plot_arc_cos() {
-    for x in 1..75 {
-        let x = x as f64;
-        let d_inv = 75. - x;
-        let d_inv_norm = 1. + -1. * (d_inv / 75.0);
-        let g = d_inv_norm;
-        // let g = g * 75.;
-        for _ in 1..(g as i32) {
-            print!(" ")
-        }
-        println!(". ({g})");
+fn distance(mouse: &MouseState, x: usize, y: usize) -> f64 {
+    let a = x.abs_diff(mouse.x as usize).pow(2);
+    let b = y.abs_diff(mouse.y as usize).pow(2);
+    let distance_squared = (a + b) as f64;
+    distance_squared.sqrt()
+}
+
+fn sample(bg: &DynamicImage, width: u32, height: u32, x: u32, y: u32) -> Color {
+    let sample_x = x * bg.width() / width;
+    let sample_y = y * bg.height() / height;
+    let rgba = bg.get_pixel(
+        sample_x.clamp(0, bg.width() - 1),
+        sample_y.clamp(0, bg.height() - 1),
+    );
+    Color {
+        r: rgba.0[0],
+        g: rgba.0[1],
+        b: rgba.0[2],
     }
+}
+
+fn project(dx: f64, dy: f64, distance: f64) -> (i32, i32) {
+    let d_norm = distance / 75.0;
+    let g = (-0.5 + d_norm).tan();
+    let dx = dx * g;
+    let dy = dy * g;
+    (dx as i32, dy as i32)
 }
