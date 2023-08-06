64e2a04beb69cde3e925373fa72940f6bafc7b50
Matte <mattias.johansson@funnel.io>
2023-08-07 00:09:48 +0200

    Voronoi

--- bunch-o-bevy-apps/voronoi/src/main.rs
+++ bunch-o-bevy-apps/voronoi/src/main.rs
@@ -15,7 +15,9 @@ fn main() {
             // }),
             Material2dPlugin::<MyMaterial>::default(),
         ))
+        .insert_resource(Material { handle: None })
         .add_systems(Startup, setup)
+        .add_systems(Update, update)
         .run();
 }
 
@@ -30,16 +32,44 @@ fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<MyMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut material: ResMut<Material>,
     q_window: Query<&Window, With<PrimaryWindow>>,
 ) {
     let window = q_window.single();
     commands.spawn(Camera2dBundle::default());
+    let handle = materials.add(MyMaterial::default());
+    material.handle = Some(handle.clone());
     commands.spawn(MaterialMesh2dBundle {
         mesh: meshes.add(Mesh::from(shape::Quad::default())).into(),
-        material: materials.add(MyMaterial::default()),
+        material: handle,
         transform: Transform::from_scale(Vec3::ONE * window.width()),
         ..default()
     });
+}
+
+const SPEED: f32 = 0.0001;
+
+fn update(mut materials: ResMut<Assets<MyMaterial>>, material: Res<Material>, time: Res<Time>) {
+    if let Some(handle) = &material.handle {
+        if let Some(mat) = materials.get_mut(handle) {
+            for vec in &mut mat.points {
+                vec.x += vec.z * time.elapsed_seconds() * SPEED;
+                if vec.x < 0.0 {
+                    vec.x += 1.0;
+                }
+                if vec.x > 1.0 {
+                    vec.x -= 1.0;
+                }
+                vec.y += vec.w * time.elapsed_seconds() * SPEED;
+                if vec.y < 0.0 {
+                    vec.y += 1.0;
+                }
+                if vec.y > 1.0 {
+                    vec.y -= 1.0;
+                }
+            }
+        }
+    }
 }
 
 #[derive(TypePath, TypeUuid, AsBindGroup, Clone)]
@@ -51,6 +81,11 @@ struct MyMaterial {
     colors: [Vec4; 100],
 }
 
+#[derive(Resource)]
+struct Material {
+    handle: Option<Handle<MyMaterial>>,
+}
+
 impl MyMaterial {
     fn default() -> MyMaterial {
         let mut rng = thread_rng();
@@ -58,8 +93,8 @@ impl MyMaterial {
         for point in &mut points {
             point.x = rng.gen();
             point.y = rng.gen();
-            point.z = rng.gen();
-            point.w = rng.gen();
+            point.z = rng.gen::<f32>() - 0.5; // dx
+            point.w = rng.gen::<f32>() - 0.5; // dy
         }
         let mut colors = [Vec4::ONE; 100];
         for color in &mut colors {
